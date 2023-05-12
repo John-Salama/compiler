a3fbe58b79c2ab3e8e37e3ef28d251930293b226
HelalOmar2001 <63786276+HelalOmar2001@users.noreply.github.com>
2023-05-12 22:46:32 +0300

    optimizing th text editor to be more responsive and be like text editor vs code

--- compiler client/script.js
+++ compiler client/script.js
@@ -1,11 +1,35 @@
 const btn = document.querySelector(".btn");
-const output = document.querySelector(".output");
-const input = document.querySelector(".input");
+var output = document.querySelector(".output");
+var input = document.querySelector(".input");
 
 btn.addEventListener("click", async function () {
-  const code = input.value;
-  console.log(code);
+  const code = inputEditor.getValue();
+  console.log(inputEditor.getValue());
   await axios.post("http://127.0.0.1:3000/runEXE", { code: code });
   const res = await axios.get("http://127.0.0.1:3000/runEXE");
-  output.textContent = res.data;
+  outputEditor.setValue(res.data);
 });
+
+/***********************************************/
+
+var inputEditor = CodeMirror.fromTextArea(input, {
+  mode: "text/x-c++src",
+  lineNumbers: true,
+  indentUnit: 4,
+  theme: "material-darker",
+  matchBrackets: true, // highlight matching brackets
+  autoCloseBrackets: true, // automatically close brackets and quotes
+});
+
+// inputEditor.setOption("mode", "text/x-c++src");
+var outputEditor = CodeMirror.fromTextArea(output, {
+  mode: "text/x-python",
+  lineNumbers: true,
+  indentUnit: 4,
+  theme: "material-darker",
+  matchBrackets: true, // highlight matching brackets
+  autoCloseBrackets: true, // automatically close brackets and quotes
+});
+// outputEditor.setOption("mode", "text/x-c++src");
+inputEditor.setSize("30%");
+outputEditor.setSize("30%");
--- compiler server/input.cpp
+++ compiler server/input.cpp
@@ -11,3 +11,42 @@ while(true){
 }else{
 	x=5;
 }
+int x=5;
+if((j>2&&j>8) || (j>2&&j>8)){
+	x=6;
+if(x==5){
+	x=6;
+while(true){
+	x=10;}
+}
+}else if(j>2 || j>8){
+	x=7;
+}else{
+	x=5;
+}
+int x=5;
+if((j>2&&j>8) || (j>2&&j>8)){
+	x=6;
+if(x==5){
+	x=6;
+while(true){
+	x=10;}
+}
+}else if(j>2 || j>8){
+	x=7;
+}else{
+	x=5;
+}
+int x=5;
+if((j>2&&j>8) || (j>2&&j>8)){
+	x=6;
+if(x==5){
+	x=6;
+while(true){
+	x=10;}
+}
+}else if(j>2 || j>8){
+	x=7;
+}else{
+	x=5;
+}
